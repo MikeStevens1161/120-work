bdb567330b482340f35b760f83653b2dd60ba97a
Mike Stevens <michael-1161@hotmail.com>
2017-11-01 21:40:18 -0600

    better

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -2,10 +2,13 @@ var bubbles;  // Declare objects
 
 //bubble
 function bubble() {
+
   this.x = random(width);
   this.y = random(height);
   this.diameter = random(10, 80);
   this.speed = 1;
+
+
 
   this.move = function() {
     this.x += random(-this.speed, this.speed);
@@ -17,18 +20,22 @@ function bubble() {
   };
 }
 function setup() {
+
   createCanvas(windowWidth, windowHeight);
+    background(150, 89, 100);
   // Create object
-  bubbles = new bubble();
+
 }
 
 function draw() {
-
+  fill(random(0,255),(0,255),(0,255));
+  bubbles = new bubble();
+  bubbles2 = new bubble();
     for (var x = 0; x <90; x++){
   bubbles.move();
   bubbles.display();
-  bubbles.move();
-  bubbles.display();
+  bubbles2.move();
+  bubbles2.display();
 }
 
 }
