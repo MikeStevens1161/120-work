92c36f3b601b2eece9842e0a34e9c563274ad7de
Mike Stevens <michael-1161@hotmail.com>
2017-11-14 19:40:01 -0700

    fixing files

---
+++ hw-10/readme.md
@@ -0,0 +1,5 @@
+This week has been a bit of a challenge but I must say I've absolutly loved it.
+I looking forward to all I'll be able to do with this animation. Wanting to go into video game designing
+I can see where this stuff will come in handy. I think my biggest challenge this week was trying to find the time
+to work on this project. If I did't have to study so much for ethics class I would have spent all my time on this project
+I theroly enjoyed it and look forward to learning to apply this stuff with a 3D effect.
---
+++ hw-10/sketch.js
@@ -0,0 +1,39 @@
+var stars = [];
+
+function setup(){
+
+  createCanvas(windowWidth, windowHeight);
+  makeStars();
+
+}
+
+function makeStars(){
+  background(0);
+
+  if (framecount < 2){
+    for(var i = 0; i < 333; i++){
+      stars.push(new Star());
+    }
+
+  }
+  strokeWeight(1);
+
+  if (framecount % 100 === 0)
+      stroke(0,0,255)
+  else
+      stroke(255);
+
+  for (var i = 0; i < stars.length; i++; )
+        point(stars[i].pos.x, stars[i].pos.y);
+}
+
+funtion Star(){
+  this.pos = createVector(Math.random()* width,
+                          Math.random()*height);
+
+}
+
+function draw(){
+makeStars();
+
+}
