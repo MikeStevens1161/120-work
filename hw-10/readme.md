a29008c95b64bf9985cee155dfb2ab92616ade7f
Mike Stevens <michael-1161@hotmail.com>
2017-11-15 01:08:41 -0700

    hw-10 complete

--- hw-10/readme.md
+++ hw-10/readme.md
@@ -1,5 +1,1 @@
-This week has been a bit of a challenge but I must say I've absolutly loved it.
-I looking forward to all I'll be able to do with this animation. Wanting to go into video game designing
-I can see where this stuff will come in handy. I think my biggest challenge this week was trying to find the time
-to work on this project. If I did't have to study so much for ethics class I would have spent all my time on this project
-I theroly enjoyed it and look forward to learning to apply this stuff with a 3D effect.
+I loved doing this assingment. Adding arrays and my own functions improved my work ten fold. I love to compare my two projects side by side. I can't wait to see what else i'll be able to do by the end of the semester. I changed my project 5 if your wanting to compare them during the grading.
--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,36 +1,60 @@
-var stars = [];
 
-function setup(){
-  createCanvas(windowWidth, windowHeight)
-  makeStars();
+//Array
+let stars = [];
+
+//my variables
+let angle = 0;
+  //earth function
+function earth(){
+    fill('blue');
+    ellipse(width*.5,height*.5,200);
+}
+function moon(){
+  translate( width*0.5, height*0.5 );
+      rotate( radians(angle));
+        fill( 'rgb(247, 247, 247)' );
+        ellipse(200,200,50);
+
+   angle = angle + 1;
 }
 
-function makeStars(){
-  background(0);
-  if (framecount < 2){
-    for(var i = 0; i < 333; i++){
-      stars.push(new Star());
+
+//setup function
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background('Black');
+  for (let i = 0; i < 1000; i++){
+    let x = random(width);
+    let y = random(height);
+    let r = 1;
+    stars[i] = new Star(x,y,r);
+  }
+}
+
+//draw function
+function draw() {
+  background('black');
+  for (let i = 0; i < stars.length; i++){
+    stars[i].show();
     }
+    earth();
+    moon();
+  }
 
+//making my objects
+class Star {
+    constructor(x,y,r){
+      this.x = x;
+      this.y = y;
+      this.r = r;
+  }
+  show(){
+    stroke(random('white','blue'));
+    strokeWeight(this.r);
+    point(this.x,this.y);
+  }
 }
 
-  strokeWeight(1);
-  if (framecount % 100 === 0)
-      stroke(0,0,255)
-  else
-      stroke(255);
+//funciion earth(){
 
-  for (var i = 0; i < stars.length; i++; )
-        point(stars[i].pos.x, stars[i].pos.y);
-}
-
-funtion Star(){
-  this.pos = createVector(Math.random()* width,
-                          Math.random()*height);
-
-}
-
-function draw(){
-makeStars();
-
-}
+//};
